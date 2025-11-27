421b96a8ea6bc06ce339df2f454dad34a6cf8034
Alexandra Domareski <aleexandra.junges@gmail.com>
2025-11-27 15:51:46 +0100

    JS lab countdown with setTimeout and setInterval

--- src/index.js
+++ src/index.js
@@ -1,38 +1,60 @@
 const DURATION = 10; // 10 seconds
 let remainingTime = DURATION; // Countdown starting from 10
 let timer = null; // Variable to store the interval
+let letTimeout = null;
 
-
+//DOM Manipulation
+const startCountdownBtn = document.getElementById("start-btn");
+const timeElement = document.getElementById("time");
+const toastCardElement = document.getElementById("toast");
+const closeToastElement = document.getElementById("close-toast");
+const toastMessageElement = document.getElementById("toast-message");
 
 // ITERATION 1: Add event listener to the start button
+startCountdownBtn.addEventListener("click", () => {
+  startCountdown();
+  remainingTime = 10;
+  timeElement.innerText = remainingTime;
+})
 
-// Your code goes here ...
-
-
-
+closeToastElement.addEventListener("click", () => {
+  clearTimeout(letTimeout)
+  toastCardElement.classList.remove("show");
+})
 
 // ITERATION 2: Start Countdown
 function startCountdown() {
-  console.log("startCountdown called!");
+  //console.log("startCountdown called!");
 
+  timer = setInterval(() => {
+    if(remainingTime === 10) {
+      showToast("⏰ Final countdown! ⏰") 
+      
+    } else if(remainingTime === 5) {
+      showToast("Start the engines! 💥") 
+    
+    } else if(remainingTime === 0) {
+      showToast("Lift off! 🚀") 
+      clearInterval(timer);
+      startCountdownBtn.disabled = false;
+      return;
+    }
 
-  // Your code goes here ...
+    remainingTime--;
+    timeElement.innerText = remainingTime;
+    startCountdownBtn.disabled = true;
+  }, 1000);
 }
-
-
 
 
 // ITERATION 3: Show Toast
 function showToast(message) {
-  console.log("showToast called!");
-
-  // Your code goes here ...
-
-
-
-
   // BONUS: ITERATION 4: TOAST CLOSE BUTTON
+  toastCardElement.classList.add("show");
 
-  // Your code goes here ...
+  toastMessageElement.innerText = `${message}`;
 
+  letTimeout = setTimeout(() => {
+    toastCardElement.classList.remove("show");
+  }, 3000);
 }
